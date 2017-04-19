60fd5f1a87bd504db794889427cd272220a7e410
洪英皓 <hauhauster@gmail.com>
2017-04-19 20:50:10 +0800

    Hello SQL queen
    SQL queen 你好

--- src/main/java/com/team03/team03/HelloServlet.java
+++ src/main/java/com/team03/team03/HelloServlet.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class HelloServlet extends GenericServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        res.getWriter().println("Hello world!!");
+        res.getWriter().println("Hello SQL queen!!");
     } 
 }
