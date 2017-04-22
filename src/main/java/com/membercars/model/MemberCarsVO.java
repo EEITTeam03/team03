7d5df57cfe12dad48a2eb08f06a5cce468bfdac0
Peta <Peta@peter>
2017-04-22 21:36:04 +0800

    修正VO
    測branch

--- src/main/java/com/membercars/model/MemberCarsVO.java
+++ src/main/java/com/membercars/model/MemberCarsVO.java
@@ -8,6 +8,24 @@ public class MemberCarsVO {
 	MemberInfoVO memberInfoVO;
 	String carLicense;
 	CarTypeVO carTypelVO;
+	public MemberInfoVO getMemberInfoVO() {
+		return memberInfoVO;
+	}
+	public void setMemberInfoVO(MemberInfoVO memberInfoVO) {
+		this.memberInfoVO = memberInfoVO;
+	}
+	public String getCarLicense() {
+		return carLicense;
+	}
+	public void setCarLicense(String carLicense) {
+		this.carLicense = carLicense;
+	}
+	public CarTypeVO getCarTypelVO() {
+		return carTypelVO;
+	}
+	public void setCarTypelVO(CarTypeVO carTypelVO) {
+		this.carTypelVO = carTypelVO;
+	}
 	
 
 }
