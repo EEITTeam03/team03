84275981db7a7a6048a71f21dae53da8f9067cd7
Peta <Peta@peter>
2017-04-22 21:34:59 +0800

    增加Member和Membercars 的VO
    順便測試branch

---
+++ src/main/java/com/membercars/model/MemberCarsVO.java
@@ -0,0 +1,13 @@
+package com.membercars.model;
+
+import com.cartype.model.CarTypeVO;
+import com.memberinfo.model.MemberInfoVO;
+
+public class MemberCarsVO {
+	
+	MemberInfoVO memberInfoVO;
+	String carLicense;
+	CarTypeVO carTypelVO;
+	
+
+}
---
+++ src/main/java/com/memberinfo/model/MemberInfoVO.java
@@ -0,0 +1,63 @@
+package com.memberinfo.model;
+
+import java.sql.Date;
+
+public class MemberInfoVO {
+	Integer memberNo;
+	String email;
+	String password;
+	String memberName;
+	String phone;
+	Date birthday;
+	String address;
+	Date effectiveDate;
+	
+	public Integer getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(Integer memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+}
