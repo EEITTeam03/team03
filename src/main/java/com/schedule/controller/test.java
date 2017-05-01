de3b01e90e3a11ff6f0eba071c1a99a248742de4
洪英皓 <@eeit9306>
2017-05-01 22:00:30 +0800

    測試圖片放入json成功

---
+++ src/main/java/com/schedule/controller/test.java
@@ -0,0 +1,63 @@
+package com.schedule.controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONValue;
+
+/**
+ * Servlet implementation class test
+ */
+@WebServlet("/admin/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		//把圖片轉byte陣列，檔案路徑要自己改要抓的圖片路徑
+		FileInputStream fis = new FileInputStream("C:/Users/user/Pictures/Saved Pictures/1.jpg");
+		byte[] b = new byte[fis.available()];
+		fis.read(b);
+		fis.close();
+		//用base64編碼
+		String img = Base64.getEncoder().encodeToString(b);
+		Map map = new HashMap();
+		map.put("img1", img);
+		//轉json
+		String jsonString = JSONValue.toJSONString(map);  
+		 out.println(jsonString);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/admin/test.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<script type="text/javascript" src = "https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		
+		$.getJSON('test',function(datas){
+			//console.log(datas.img1);
+			var x = $("<img/>");
+			x.attr('src','data:image/jpeg;base64,'+datas.img1);
+			x.attr("width","180");
+			x.attr("height","180");
+			$('body').append(x);
+			})
+		});
+	
+	</script>
+	
+</body>
+</html>
