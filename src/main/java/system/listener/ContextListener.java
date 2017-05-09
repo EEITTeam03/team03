97cb54fd5c6bc2b9c429e62f2268e2b05d8e6789
洪英皓 <hauhauster@gmail.com>
2017-05-09 20:11:53 +0800

    Bang Bang Bang

--- src/main/java/system/listener/ContextListener.java
+++ src/main/java/system/listener/ContextListener.java
@@ -35,29 +35,29 @@ public class ContextListener implements ServletContextListener {
     	
     	/**********塞入今日預約名單(for VideoServlet)----------------開始*************/
     	/**********ApplicationListener伺服器啟動時呼叫排程任務ScheduleTask*************/
-    	Calendar reSetTime = Calendar.getInstance();
-    	//隔天的半夜一點
-    	reSetTime.add(Calendar.DATE, 1);
-    	reSetTime.set(Calendar.HOUR_OF_DAY, 1);
-    	reSetTime.set(Calendar.MINUTE, 0);
-    	reSetTime.set(Calendar.SECOND, 0);
-    	reSetTime.set(Calendar.MILLISECOND, 0);
-    	Long reSetMillis = reSetTime.getTimeInMillis();
-    	
-    	Calendar now = Calendar.getInstance();
-    	Long nowMillis = now.getTimeInMillis();
-    	
-    	
-    	//這個是排程
-    	ScheduleTask schedule = new ScheduleTask(event.getServletContext());
-    	//schedule.addTask(多久以後去做  , 間隔多久做一次)
-    	schedule.addTask(reSetMillis-nowMillis, 1000*60*60*24);
-    	//schedule.addTask(1000, 10000);//測試用
-    	
-    	//這個是今天的
-    	AutoSetTodayReservList s = new AutoSetTodayReservList();
-		s.TodayReservListInit(event.getServletContext());
-		s.TodayReservListQuery(event.getServletContext());
+//    	Calendar reSetTime = Calendar.getInstance();
+//    	//隔天的半夜一點
+//    	reSetTime.add(Calendar.DATE, 1);
+//    	reSetTime.set(Calendar.HOUR_OF_DAY, 1);
+//    	reSetTime.set(Calendar.MINUTE, 0);
+//    	reSetTime.set(Calendar.SECOND, 0);
+//    	reSetTime.set(Calendar.MILLISECOND, 0);
+//    	Long reSetMillis = reSetTime.getTimeInMillis();
+//    	
+//    	Calendar now = Calendar.getInstance();
+//    	Long nowMillis = now.getTimeInMillis();
+//    	
+//    	
+//    	//這個是排程
+//    	ScheduleTask schedule = new ScheduleTask(event.getServletContext());
+//    	//schedule.addTask(多久以後去做  , 間隔多久做一次)
+//    	schedule.addTask(reSetMillis-nowMillis, 1000*60*60*24);
+//    	//schedule.addTask(1000, 10000);//測試用
+//    	
+//    	//這個是今天的
+//    	AutoSetTodayReservList s = new AutoSetTodayReservList();
+//		s.TodayReservListInit(event.getServletContext());
+//		s.TodayReservListQuery(event.getServletContext());
     	
     	
 		//若攝影機VideoServlet的ip從WEB-INF/properties/team03.properties讀取	    	
