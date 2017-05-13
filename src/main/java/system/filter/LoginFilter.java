9b632d0e18b6d80cd89cd9b2ea6cec36f32f499a
周柏元 <pc@eeit>
2017-05-13 17:01:42 +0800

    做好登入控管頁面 (FB登入怪怪)

---
+++ src/main/java/system/filter/LoginFilter.java
@@ -0,0 +1,57 @@
+package system.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.memberinfo.model.MemberInfoVO;
+
+
+@WebFilter(urlPatterns={"/start_reserve.jsp"})
+public class LoginFilter implements Filter {
+
+
+    public LoginFilter() {
+        System.out.println("**********LoginFilter initialized**********");
+    }
+
+
+	public void destroy() {
+		 System.out.println("**********LoginFilter destroyed**********");
+	}
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest httpReq =(HttpServletRequest) request;
+		HttpServletResponse httpResp = (HttpServletResponse) response;
+		
+		MemberInfoVO member = (MemberInfoVO) httpReq.getSession().getAttribute("memberInfo");
+		
+		//判斷他是否有登入，如果沒有---->導向登入頁面
+		if(member==null) {
+			httpReq.getSession().setAttribute("target", httpReq.getContextPath()+"/start_reserve.jsp");
+			httpResp.sendRedirect(httpReq.getContextPath()+"/login.jsp");
+		}
+//		httpReq.getSession().setAttribute("orginURL", httpReq.getRequestURI());
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		
+		
+		
+	}
+
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
